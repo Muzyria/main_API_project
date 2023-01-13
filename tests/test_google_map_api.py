d0807e1598d37b8997cf5d17393b006a751f410b
“Muzyria” <fila090580@gmail.com>
2023-01-13 12:42:20 +0200

    new

---
+++ tests/test_google_map_api.py
@@ -0,0 +1,11 @@
+from requests import Response
+
+from utilities.api import Google_maps_api
+
+"""Создание, изменения и удаления новой локации"""
+
+class Test_create_place:
+
+    def test_create_new_place(self):
+
+        result_post: Response = Google_maps_api.create_new_place()
