4c1731db77ecea122bd17963d037ed12d1fa16e2
“Muzyria” <fila090580@gmail.com>
2023-01-13 16:10:38 +0200

    new

--- tests/test_google_map_api.py
+++ tests/test_google_map_api.py
@@ -9,9 +9,9 @@ class Test_create_place:
     def test_create_new_place(self):
 
         print("Метод POST")
-        result_post: Response = Google_maps_api.create_new_place()
+        result_post = Google_maps_api.create_new_place()
         check_post = result_post.json()
         place_id = check_post.get("place_id")
 
         print("Метод POST")
-        result_post: Response = Google_maps_api.get_new_place(place_id)
+        result_post = Google_maps_api.get_new_place(place_id)
