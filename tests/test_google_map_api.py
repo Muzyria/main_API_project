854b4c7038910f1ecb4b86fd98af3e2dc3860375
Muzyria <fila090580@gmail.com>
2023-01-15 01:26:21 +0200

    new

--- tests/test_google_map_api.py
+++ tests/test_google_map_api.py
@@ -1,5 +1,5 @@
-from requests import Response
 
+from utilities.checking import Cheking
 from utilities.api import Google_maps_api
 
 """Создание, изменения и удаления новой локации"""
@@ -12,18 +12,24 @@ class Test_create_place:
         result_post = Google_maps_api.create_new_place()
         check_post = result_post.json()
         place_id = check_post.get("place_id")
+        Cheking.check_status_code(result_post, 200)
 
         print("Метод GET POST")
         result_get = Google_maps_api.get_new_place(place_id)
+        Cheking.check_status_code(result_get, 200)
 
         print("Метод PUT")
         result_put = Google_maps_api.put_new_place(place_id)
+        Cheking.check_status_code(result_put, 200)
 
         print("Метод GET PUT")
         result_get = Google_maps_api.get_new_place(place_id)
+        Cheking.check_status_code(result_get, 200)
 
         print("Метод DELETE")
         result_delete = Google_maps_api.delete_new_place(place_id)
+        Cheking.check_status_code(result_delete, 200)
 
         print("Метод GET DELETE")
         result_get = Google_maps_api.get_new_place(place_id)
+        Cheking.check_status_code(result_get, 200)
