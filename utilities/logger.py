1bdb69efd8324fe96246a24f0b752b8a63df9e0e
Muzyria <fila090580@gmail.com>
2023-01-15 20:26:50 +0200

    new

--- utilities/logger.py
+++ utilities/logger.py
@@ -1,6 +1,6 @@
 import datetime
 import os
-
+from requests import Response
 
 class Logger():
     file_name = fr"C:\Git_Muzyria\main_project\logs\log_" + str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")) + ".log"
@@ -11,23 +11,28 @@ class Logger():
             logger_file.write(data)
 
     @classmethod
-    def add_start_step(cls, method: str):
+    def add_request(cls, url: str, method: str):
         test_name = os.environ.get('PYTEST_CURRENT_TEST')
 
         data_to_add = f"\n-----\n"
         data_to_add += f"Test: {test_name}\n"
-        data_to_add += f"Start time: {str(datetime.datetime.now())}\n"
-        data_to_add += f"Start name method: {method}\n"
+        data_to_add += f"Time: {str(datetime.datetime.now())}\n"
+        data_to_add += f"Request method: {method}\n"
+        data_to_add += f"Request URL: {url}\n"
         data_to_add += "\n"
 
         cls.write_log_to_file(data_to_add)
 
     @classmethod
-    def add_end_step(cls, url: str, method: str):
+    def add_response(cls, result: Response):
 
-        data_to_add = f"End time: {str(datetime.datetime.now())}\n"
-        data_to_add += f"End name method: {method}\n"
-        data_to_add += f"URL: {url}\n"
+        cookies_as_dict = dict(result.cookies)
+        headers_as_dict = dict(result.headers)
+
+        data_to_add = f"Response code: {result.status_code}\n"
+        data_to_add += f"Response text: {result.text}\n"
+        data_to_add += f"Response cookies: {cookies_as_dict}\n"
+        data_to_add += f"Response headers: {headers_as_dict}\n"
         data_to_add += f"\n-----\n"
 
         cls.write_log_to_file(data_to_add)
